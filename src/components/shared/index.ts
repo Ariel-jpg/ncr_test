3abe1bd3944edec672952226e1f491d028117365
Ariel-jpg <epofmip@gmail.com>
2023-08-05 14:38:53 -0300

    added barrel files

---
+++ src/components/shared/index.ts
@@ -0,0 +1,3 @@
+export { AccountOptions, type AvaiableAccountTypes, TypeCurrencyOptions, type TypeCurrencyTypes, type AccountType } from "./types";
+export { default as getAccountString } from "./functions/getAccountString";
+export * from "./Test.wrapper";
